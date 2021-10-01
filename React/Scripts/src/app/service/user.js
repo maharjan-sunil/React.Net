c3452bfd90fbfc06cd00679ab7d049c0eed5c2b1
Sunil Maharjan <sunil.maharjan@cedargate.com>
2021-10-01 15:21:33 +0545

    UI-Integration added web api

---
+++ React/Scripts/src/app/service/user.js
@@ -0,0 +1,11 @@
+﻿import axios from "axios";
+
+let userApi = {
+    getAllUsers: getAllUsers
+}
+
+function getAllUsers() {
+    return axios.get('https://localhost:44331/api/user');
+}
+
+export default userApi;
--- React/Scripts/src/index.js
+++ React/Scripts/src/index.js
@@ -1,13 +1,19 @@
 ﻿import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "mobx-react";
+import { HashRouter } from "react-router-dom";
 
-import Dashboard from './app/dashboard/index.jsx';
-//import History from './app/dashboard/history.jsx';
+import App from './App.js';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const App = () => <Dashboard name="sunil" />
+import './css/site.css'
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
-//const HistoryDirective = () => <History name="manish" />
-
-//ReactDOM.render(<HistoryDirective />, document.getElementById("history"));
+ReactDOM.render(
+    <Provider>
+        <HashRouter>
+            <App />
+        </HashRouter>
+        <ToastContainer />
+    </Provider>,
+    document.getElementById('react-app'));
